713eb9535755e98c9f4c435941acd74e2b27e278
seraphinandrieux <seraphin.andrieux@cpe.fr>
2019-10-17 17:53:44 +0200

    test

---
+++ app/src/main/java/cpe/mobile/mybrowzik/adapters/AudioFileListAdapter.java
@@ -0,0 +1,53 @@
+package cpe.mobile.mybrowzik.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cpe.mobile.mybrowzik.AudioFile;
+import cpe.mobile.mybrowzik.R;
+import cpe.mobile.mybrowzik.databinding.AudioFileItemBinding;
+
+public class AudioFileListAdapter extends RecyclerView.Adapter<AudioFileListAdapter.ViewHolder>{
+    List<AudioFile> audioFileList;
+    public AudioFileListAdapter(List<AudioFile> fileList) {
+        assert fileList != null;
+        audioFileList = fileList;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+        {
+            AudioFileItemBinding binding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    R.layout.audio_file_item, parent,false
+            );
+            return new ViewHolder(binding);
+        }
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            AudioFile file = audioFileList.get(position);
+            holder.binding.title.setText(file.getTitle());
+            holder.binding.artist.setText(file.getArtist());
+            holder.binding.album.setText(file.getAlbum());
+            holder.binding.duration.setText(file.getDurationText());
+        }
+
+        @Override
+        public int getItemCount() {
+            return audioFileList.size();
+        }
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            private AudioFileItemBinding binding;
+            ViewHolder(AudioFileItemBinding binding) {
+                super(binding.getRoot());
+                this.binding = binding;
+            }
+        }
+}
